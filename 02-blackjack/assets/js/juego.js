3d402f87993e0927d2366d98acf762bc8f58d313
George Mota <gamcustodio@outlook.com>
2023-05-22 16:10:34 -0400

    Funtion add: Modal

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -1,45 +1,45 @@
-//Values carts
 /*
- * 2C = Two of Clubs 
- * 2D = Two of Diamonds
- * 2H = Two of Hearts
- * 2S = Two of Spades
- */
+*******************************************************************************************
+General rules
+1- The house does not tie.
+2- You must match the counter to 21.
+3- You must get closer 21 than the computer.Types carts
+2C = Two of clovers, 2D = Two of diamonds, 2H = Two of Hearts, 2S = Two of Spades
 
-//Este comentario es para certificar el correcto funcionamiento de mis commits
+Values carts
+from 2 to 9, the value is the same.
+from 10 to K, the value is 10.
+the value for As is 11.
+*******************************************************************************************
+*/
+//Variables
+let deck = [], countPlayer = 0, countComputer = 0, resultGameString = '', winsGifts = ['win.gif', 'win1.gif', 'win2.gif', 'win3.gif'], losesGifts = ['lose.gif', 'lose1.gif','lose3.gif'];
+const tipos = ['C','D','H','S'], especiales = ['A','J','Q','K'];
 
-let deck = [],
-    countPlayer = 0,
-    countComputer = 0;
-const tipos = ['C','D','H','S'],
-especiales = ['A','J','Q','K'];
-    
-//HTML References location 
+//HTML Reference locations
 const btnNewGame = document.querySelector('#btnNewGame');
 const btnRequestCart = document.querySelector('#btnRequestCart');
 const btnStop = document.querySelector('#btnStop');
 const smallPlayer = document.querySelectorAll('small');
 const carts = document.querySelector('#jugador-cartas');
 const cartsComputer = document.querySelector('#computadora-cartas');
+const btnOpenModal = document.querySelector('#btnOpenModal');
+const btnCloseModal = document.querySelector('#btnCloseModal');
+const modalLocation = document.querySelector('#modalLocation');
+const modalText = document.querySelector('#modalText');
 
 //This function allows me create a new deck
-
 const crearDeck = () => {
-
     for ( let i = 2; i <= 10; i++) {
         for( let tipo of tipos ){
             deck.push(i + tipo);
         }
-    
     }
-
     for( let tipo of tipos ) {
         for( let esp of especiales ){
             deck.push(esp + tipo);
-        }
-    
+        } 
     }
-
     deck= _.shuffle(deck);
     return deck;
 }
@@ -47,7 +47,6 @@ const crearDeck = () => {
 crearDeck();
 
 //This function allows me to get a card from the deck
-
 const pedirCarta = () => {
 
     if ( deck.length === 0 ) {
@@ -57,14 +56,37 @@ const pedirCarta = () => {
     return carta;
 }
 
-
 const valorCarta = ( carta ) => {
     const valor = carta.substring(0, carta.length - 1);
     return ( isNaN(valor)) ? ( valor === 'A' ) ? 11 : 10 : valor * 1;
 }
 
-//Events
+const randomItemWins = (winsGifts) =>
+    {
+    return winsGifts[Math.floor(Math.random()*winsGifts.length)];
+    }
 
+const randomItemLoses = (losesGifts) =>
+    {
+    return losesGifts[Math.floor(Math.random()*losesGifts.length)];
+    }
+
+//This function return the result game
+const resultGame = (loseOrWin) => {
+    modal.showModal();
+    const resultGame = document.createElement("img");
+    if (loseOrWin === 'Perdiste'){
+        resultGame.src =`assets/backgroups/loses/${randomItemLoses(losesGifts)}`;
+        modalText.innerText = "U'RE A FUCKING LOSER";
+    } else {
+        resultGame.src =`assets/backgroups/wins/${randomItemWins(winsGifts)}`;
+        modalText.innerText = "U'RE A FUCKING WINER";
+    }
+    resultGame.classList.add('modal-container');
+    modalLocation.append(resultGame);
+}
+
+//Events
 const turnoComputadora = ( puntosMinimos ) => {
 
     do {  
@@ -75,50 +97,49 @@ const turnoComputadora = ( puntosMinimos ) => {
         addCart.src = `assets/cartas/${carta}.png`;
         addCart.classList.add('carta');
         cartsComputer.append(addCart);
-
         if( puntosMinimos > 21 ) {
             break;
         } 
-        
     } while ( (countComputer < puntosMinimos) && (puntosMinimos <= 21) );
 
     setTimeout(() => {
 
         if( countComputer === puntosMinimos ){
-            alert('Perdiste, la casa no empata.');
+            //resultGameString = 'Perdiste, la casa no empata';
+            resultGameString = 'Perdiste';
+            resultGame(resultGameString);
         } else if ( puntosMinimos > 21 ){
-            alert('Perdiste');
+            resultGameString = 'Perdiste';
+            resultGame(resultGameString);
         } else if ( countComputer > 21) {
-            alert('Ganaste');
+            resultGameString = 'Ganaste';
+            resultGame(resultGameString);
         } else if ( countPlayer < countComputer && countComputer <= 21) {
-            alert('Perdiste');
+            resultGame();
         }
-    }, 10 );
-  
+    }, 50 );
 }
 
 //Request card
-
 btnRequestCart.addEventListener('click', () => {
-
     const carta = pedirCarta();
     const addCart = document.createElement("img");
     addCart.src = `assets/cartas/${carta}.png`;
     addCart.classList.add('carta');
     carts.append(addCart);
     countPlayer += valorCarta(carta);
-
-    //Update countPlayer
+//Update countPlayer
     smallPlayer[0].innerText = countPlayer * 1; 
-    
     if (countPlayer > 21) { 
-        console.warn ('Perdiste');
+        resultGameString = 'Perdiste';
+        resultGame(resultGameString);
         btnRequestCart.disabled = true;
         btnStop.disabled = true;
         turnoComputadora( countPlayer );
-
     } else if (countPlayer === 21) {
-        console.warn('Ganaste');
+        //alert('Ganaste');
+        resultGameString = 'Ganaste';
+        resultGame(resultGameString);
         btnRequestCart.disabled = true;
         btnStop.disabled = true;
         turnoComputadora( countPlayer );
@@ -126,20 +147,17 @@ btnRequestCart.addEventListener('click', () => {
 })
 
 //Stopped
-
 btnStop.addEventListener('click', () => {
-
-    btnStop.disabled = true;
-    btnRequestCart.disabled = true;
     if (countPlayer === 0) {
-        console.warn('No puedes detener lo que no ha iniciado.')
+        alert('No puedes detener lo que no ha iniciado.');
     } else if (countPlayer != 0){
         turnoComputadora (countPlayer);
+        btnStop.disabled = true;
+        btnRequestCart.disabled = true;
     }
 })
 
 btnNewGame.addEventListener('click', () => {
-
     console.clear();
     deck = [];
     deck = crearDeck();
@@ -152,3 +170,13 @@ btnNewGame.addEventListener('click', () => {
     btnRequestCart.disabled = false;
     btnStop.disabled = false;
 })
+
+btnOpenModal.addEventListener('click', () => {
+   resultGame();
+})
+
+btnCloseModal.addEventListener('click', () => {
+    modal.close();
+    modalLocation.innerHTML = ('');
+})
+
